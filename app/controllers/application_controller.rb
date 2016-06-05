905d4b703e8d66c55eee4a043c2af6f48df8325f
Nikolay Lipovtsev <n.lipovtsev@gmail.com>
2016-06-05 14:29:08 +0300

    Add sign up view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,54 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
+  include SessionsHelper
+  
   protect_from_forgery with: :exception
+  before_action :set_locale
+  before_filter :set_stamper
+  after_filter  :reset_stamper
+  
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
+  
+  private
+  
+  # Before filters
+  
+  def set_locale
+    I18n.locale = params[:locale] || current_user.try(:locale) || I18n.default_locale
+  end
+  
+  # The <tt>set_stamper</tt> method as implemented here assumes a couple
+  # of things. First, that you are using a +User+ model as the stamper
+  # and second that your controller has a <tt>current_user</tt> method
+  # that contains the currently logged in stamper. If either of these
+  # are not the case in your application you will want to manually add
+  # your own implementation of this method to the private section of
+  # the controller where you are including the Userstamp module.
+  def set_stamper
+    User.stamper = self.current_user
+  end
+
+  # The <tt>reset_stamper</tt> method as implemented here assumes that a
+  # +User+ model is being used as the stamper. If this is not the case then
+  # you will need to manually add your own implementation of this method to
+  # the private section of the controller where you are including the
+  # Userstamp module.
+  def reset_stamper
+    User.reset_stamper
+  end
+  
+  protected
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".logged_in_user.flash.danger.message"
+      redirect_to login_url
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,144 @@
 class User < ActiveRecord::Base
+  
+  include Userstampable::Stampable
+  include Userstampable::Stamper
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_NAME_REGEX  = /(\A\z)|(\A[[:alpha:]]+[[:alpha:] \-']*[[:alpha:]]+\z)/i
+  AVATAR_PATH       = "/system/:class/:attachment/:id_partition/:style"
+  
+  attr_accessor :remember_token
+  
+  has_many :bank_users
+  has_many :banks, through: :bank_users
+  has_secure_password
+  has_attached_file :avatar, 
+    { 
+      styles: { original: "120x120#", medium: "50x50#", thumb: "38x38#" }, 
+      default_url: "/images/:class/:attachment/:style/missing.png",
+      url: "#{AVATAR_PATH}/:hash.:extension",
+      path: ":rails_root/public#{AVATAR_PATH}/:hash.:extension",
+      hash_data: AVATAR_PATH,
+      hash_secret: "A13S4Dtu54y5g63d363sa3JH30Ff9dyH56lih6JguyHY736crtyf45dx"
+    }
+     
+  validates :last_name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+  validates :middle_name, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+  validates :gender, presence: true, length: { maximum: 6 }
+  validates_date :birth_date, presence: true, on_or_before: lambda { User.not_younger }, on_or_after: lambda { User.not_older }
+  validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password
+  validates :password_confirmation, presence: true, if: :password_confirmation
+  validates :role, presence: true, if: :role
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  before_save { email.downcase! }
+  before_save :set_name
+  after_initialize :set_default_role, if: :new_record?
+  
+  enum gender: [:male, :female, :other]
+  enum role: [:admin, :bank_user, :user]
+  
+  class << self
+    
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    # Users can not be older than this date.
+    def not_older
+      104.years.ago
+    end
+    
+    # Users can not be younger than this date.
+    def not_younger
+      14.years.ago
+    end
+  end
+  
+  # Returns the user's age.
+  def age
+    if self.birth_date
+      now = Date.today
+      now.year - self.birth_date.year - ((now.month > self.birth_date.month || (now.month == self.birth_date.month && now.day >= self.birth_date.day)) ? 0 : 1)
+    end
+  end
+  
+  # Returns true if birthday today.
+  def birthday_today?
+    if self.birth_date
+      now = Date.today
+      now.month == self.birth_date.month && now.day == self.birth_date.day
+    end
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute :remember_digest, nil
+  end
+  
+  # Returns short user name like "Fred S."
+  def short_name
+    "#{self.first_name} #{self.last_name[0]}."
+  end
+  
+  # Returns full user name like "Fred Smitt"
+  def full_name(options = {})
+    options[:middle_name]     ||= true
+    options[:last_name_first] ||= true
+    name = "#{self.first_name}"
+    name << " #{self.middle_name}" if options[:middle_name] && middle_name
+    options[:last_name_first] ? "#{self.last_name} #{name}" : "#{name} #{self.last_name}"
+  end
+  
+  # Save and returns new random password.
+  def generate_password
+    random_password             = SecureRandom.hex(5)
+    self.password               = random_password
+    self.password_confirmation  = random_password
+  end
+  
+  private
+  
+  def set_default_role
+    self.role ||= :user
+  end
+  
+  def set_name
+    self.last_name    = normalize_name self.last_name
+    self.first_name   = normalize_name self.first_name
+    self.middle_name  = normalize_name self.middle_name
+  end
+
+  def normalize_name(name)
+    if name.blank?
+      nil
+    else
+      name.gsub!(/^[ -]*|[ -]*$/, "")
+      name.gsub!(/[ ]*[-]{2,}[ ]*/, "-")
+      name.gsub!(/[ ]{2,}/, " ")
+      name.gsub!(/[[:alpha:]]+/i) { |s| s.capitalize }
+    end
+  end
 end
