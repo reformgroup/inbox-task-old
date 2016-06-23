842fc767f5150ccd6d2c63e49c3da2958f7852b0
Nikolay Lipovtsev <n.lipovtsev@gmail.com>
2016-06-23 22:34:34 +0300

    Start refactoring spec

--- spec/features/signup_pages_spec.rb
+++ spec/features/signup_pages_spec.rb
@@ -3,59 +3,78 @@ include ApplicationHelper
 include CssSpecHelper
 include SessionsSpecHelper
 
-RSpec.feature "signup pages", type: :features do
+RSpec.feature "#signup", type: :features do
   
-  scenario "signup page" do
+  before { visit signup_path }
+  let(:submit) { "Зарегистрироваться" }
+  
+  context "page" do
     
-    visit signup_path
-
-    expect(page).to have_link("Регистрация")
-    expect(page).to have_title(full_title("Регистрация"))
+    scenario { expect(page).to have_link("Регистрация") }
+    scenario { expect(page).to have_title(full_title("Регистрация")) }
   end
   
-  context "signup" do
-    
-    
+  describe "signup" do
     
     context "with invalid information" do
       
-      visit signup_path
-      
-      subject { click_button "Зарегистрироваться" }
-      scenario { expect(page).to have_title(full_title("Регистрация")) }
-    end
-    
-    scenario "with invalid information" do
-      
-      visit signup_path
-      
-      expect { click_button "Зарегистрироваться" }.not_to change(User, :count)
+      expect{ click_button submit }.not_to change(User, :count)
       expect(page).to have_title(full_title("Регистрация"))
       expect(page).to have_selector(li_danger_css)
     end
-    
-    scenario "with valid information" do
-      
-      visit signup_path
-      
-      @user = build(:user)
-      
-      fill_in "user[first_name]",                         with: @user.first_name
-      fill_in "user[last_name]",                          with: @user.last_name
-      fill_in "user[email]",                              with: @user.email
-      fill_in "user[password]",                           with: @user.password
-      fill_in "user[password_confirmation]",              with: @user.password
-      select I18n.l(@user.birth_date, format: "%-d"), from: "user_birth_date_3i"
-      select I18n.l(@user.birth_date, format: "%B"),  from: "user_birth_date_2i"
-      select I18n.l(@user.birth_date, format: "%Y"),  from: "user_birth_date_1i"
-      choose "user_gender_male"
-      
-      click_button "Зарегистрироваться"
-      
-      expect(page).to have_title(full_title(@user.short_name))
-      expect(page).to have_content(@user.name)
-      expect(page).to have_selector(alert_success_css)
-      expect(page).to have_link("Выйти")
-    end
   end
+  
+  #
+  #
+  #
+  #
+  #
+  # scenario "signup page" do
+  #
+  #   visit signup_path
+  #
+  #   expect(page).to have_link("Регистрация")
+  #   expect(page).to have_title(full_title("Регистрация"))
+  # end
+  #
+  # describe "signup" do
+  #
+  #   scenario "with invalid information" do
+  #
+  #     visit signup_path
+  #
+  #     expect{ click_button "Зарегистрироваться" }.not_to change(User, :count)
+  #     expect(page).to have_title(full_title("Регистрация"))
+  #     expect(page).to have_selector(li_danger_css)
+  #   end
+  #
+  #   scenario "with valid information" do
+  #
+  #     visit signup_path
+  #
+  #     @user = build(:user)
+  #
+  #     fill_in "user[first_name]",                         with: @user.first_name
+  #     fill_in "user[last_name]",                          with: @user.last_name
+  #     fill_in "user[email]",                              with: @user.email
+  #     fill_in "user[password]",                           with: @user.password
+  #     fill_in "user[password_confirmation]",              with: @user.password
+  #     select I18n.l(@user.birth_date, format: "%-d"), from: "user_birth_date_3i"
+  #     select I18n.l(@user.birth_date, format: "%B"),  from: "user_birth_date_2i"
+  #     select I18n.l(@user.birth_date, format: "%Y"),  from: "user_birth_date_1i"
+  #     choose "user_gender_male"
+  #
+  #     expect{ click_button "Зарегистрироваться" }.to change(User, :count).by(1)
+  #
+  #     context "after submission" do
+  #
+  #       before { click_button "Зарегистрироваться" }
+  #
+  #       expect(page).to have_title(full_title(@user.short_name))
+  #       expect(page).to have_content(@user.name)
+  #       expect(page).to have_selector(alert_success_css)
+  #       expect(page).to have_link("Выйти")
+  #     end
+  #   end
+  # end
 end
